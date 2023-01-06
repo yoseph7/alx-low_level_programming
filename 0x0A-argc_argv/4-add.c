ab47cf6ee2ec6d7fee8c7bc7b33c7046c660d6bc
yoseph7 <117742304+yoseph7@users.noreply.github.com>
2023-01-06 11:59:55 +0300

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -43,24 +43,21 @@ int main(int argc, char *argv[])
 	int sum = 0;
 
 	 count = 1;
- 	 while (count < argc) /*Goes through the whole array*/
-
-	 {
-		 if (check_num(argv[count]))
-
-		 {
-			 str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
-			 sum += str_to_int;
-		 }
-		 /*Condition if one of the number contains symbols that are not digits*/
-		 else
-		 {
-			 printf("Error\n");
-			 return (1);
-		 }
-		 count++;
-	 }
-	 printf("%d\n", sum);/*print sum*/
-
-	 return (0);
+	while (count < argc) /*Goes through the whole array*/
+	{
+		if (check_num(argv[count]))
+		{
+			str_to_int = atoi(argv[count]); /*ATOI --> covert string to int*/
+			sum += str_to_int;
+		}
+		/*Condition if one of the number contains symbols that are not digits*/
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		count++;
+	}
+	printf("%d\n", sum); /*print sum*/
+	return (0);
 }
